5e4b4ced8ddf4d2b3cf9f852b4c4451b110da5dc
proliner <proliner@DESKTOP-QSJ92T2>
2026-03-26 12:34:35 +0200

    Added Fields to the Reiews class

--- LimpopoTourDestinations/Models/Domain/Review.cs
+++ LimpopoTourDestinations/Models/Domain/Review.cs
@@ -2,6 +2,11 @@
 {
     public class Review
     {
-
+        public Guid Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+        public Guid TourId { get; set; }
     }
 }

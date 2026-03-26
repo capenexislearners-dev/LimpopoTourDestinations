8906088cde67af352fd5672e17972911a0bd86b9
proliner <proliner@DESKTOP-QSJ92T2>
2026-03-26 14:47:50 +0200

    Created a DTOs folder and Added CreateDTO and TourDTO classes and Added an ImageUrl in Tour Class

---
+++ LimpopoTourDestinations/DTOs/CreateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace LimpopoTourDestinations.DTOs
+{
+    public class CreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+        public string TourImageUrl { get; set; }
+        public int GuideId { get; set; }
+    }
+}
